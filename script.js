5ed95f8925a5d63ae5173c83440e4ef06dfdee58
Khurshid2885 <118127829+Khurshid2885@users.noreply.github.com>
2023-11-27 09:53:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function subscribe_button() {
+    document.getElementById('e-mail').value = '';
+}
+
+function send_button() {
+    document.getElementById('name').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
+}
